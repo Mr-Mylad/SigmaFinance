6fdd1d8b24c6573cb213482a7b7aeacfb4a7cf16
Mr-Mylad <mrmylad10@gmail.com>
2024-09-26 13:00:51 +0530

    pray to g o d

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # We are cooked
+
+with open("save.txt", "r") as saveFile:
+    if saveFile.read() == False:
+        print("Hi! What\'s your name?")
+
+def inputtingChat():
+    userInput = input()
+    
+    with open("save.txt", "a") as saveFile:
+        saveFile.write(f"User: {userInput}")
